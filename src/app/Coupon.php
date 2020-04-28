a2a3705e27ca5f28b22ac07cb38a06a1a7c85d4b
Kota-Yamashita <kota_yamashita@r.recruit.co.jp>
2020-04-28 13:59:12 +0900

    [fix] check coupon lllegal value

--- src/app/Coupon.php
+++ src/app/Coupon.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Coupon extends Model
 {
-    //
+    protected $table = 'coupons';
+
+    public function hasCoupon($discount) {
+        return DB::table($this->table)->where('discount', $discount)->exists();
+    }
 }
